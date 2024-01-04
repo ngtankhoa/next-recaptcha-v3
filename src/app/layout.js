10f26a9f2dad8653f17d30034cd604a77d5f28f7
John Nguyen <john.nguyen@legato.co>
2024-01-04 14:13:15 +0700

    recaptcha V3

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +11,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
+      <Script src='https://www.google.com/recaptcha/api.js'></Script>
       <body className={inter.className}>{children}</body>
     </html>
   )

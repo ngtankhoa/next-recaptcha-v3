81e615edafd2989f7c92ad765ffeead2762d321e
John Nguyen <john.nguyen@legato.co>
2024-01-04 14:26:56 +0700

    implement other way

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <Script src='https://www.google.com/recaptcha/api.js'></Script>
+      <Script src='https://www.google.com/recaptcha/api.js?render=6Le7UkUpAAAAALu8RK5I6sgdQ4ZHnY1ZBVAbjXV-'></Script>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,22 @@
+'use client'
+
 import Image from 'next/image'
 
 export default function Home() {
+  const onClick = (e) => {
+    e.preventDefault()
+    grecaptcha.ready(function () {
+      grecaptcha.execute('6Le7UkUpAAAAALu8RK5I6sgdQ4ZHnY1ZBVAbjXV-', { action: 'submit' }).then(function (token) {
+        // Add your logic to submit to your backend server here.
+        window.console.log('call backend logic here')
+      })
+    })
+  }
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
-        <button
-          className='g-recaptcha'
-          data-sitekey='6Le7UkUpAAAAALu8RK5I6sgdQ4ZHnY1ZBVAbjXV-'
-          data-callback='onSubmit'
-          data-action='submit'
-        >
-          Submit
-        </button>
+        <button onClick={onClick}>Submit</button>
       </div>
     </main>
   )
